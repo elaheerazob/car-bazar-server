58e42932f3bb375927af717f40dac0999fcdab1d
elaheerazob <ridwanelahee282@gmail.com>
2022-05-07 09:06:31 +0600

    cars id add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 
@@ -20,12 +20,21 @@ async function run () {
     try{
         await client.connect();
         const carsCollection = client.db("car-bazar").collection("cars");
+
         app.get("/cars", async (req, res) => {
           const query = {};
           const cousor = carsCollection.find(query);
           const cars = await cousor.toArray();
           res.send(cars);
         });
+
+        app.get('/cars/:id', async (req, res) => {
+          const id = req.params.id;
+          console.log(id);
+          const query = {_id: ObjectId(id)};
+          const cars = await carsCollection.findOne(query);
+          res.send(cars);
+      });
     }
     finally{
 

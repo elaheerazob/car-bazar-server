8d236e7d83f6ac35c1b42c8c027cc81173cb7c1b
elaheerazob <ridwanelahee282@gmail.com>
2022-05-28 17:25:15 +0600

    datas id add

--- index.js
+++ index.js
@@ -16,45 +16,46 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-async function run () {
-    try{
-        await client.connect();
-        const carsCollection = client.db("car-bazar").collection("cars");
+async function run() {
+  try {
+    await client.connect();
+    const carsCollection = client.db("car-bazar").collection("cars");
+    //cars
+    app.get("/cars", async (req, res) => {
+      const products = await carsCollection.find({}).toArray();
+      res.send(products);
+    });
 
-        app.get("/cars", async (req, res) => {
-          const query = {};
-          const cousor = carsCollection.find(query);
-          const cars = await cousor.toArray();
-          res.send(cars);
-        });
+    //delete
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const products = await carsCollection.deleteOne(query);
+      console.log(products);
+      res.send(products);
+    });
 
-        app.get('/cars/:id', async (req, res) => {
-          const id = req.params.id;
-          console.log(id);
-          const query = {_id: ObjectId(id)};
-          const cars = await carsCollection.findOne(query);
-          res.send(cars);
-      });
+    //post
+    app.post("/addcars", async (req, res) => {
+      const cars = req.body;
+      const products = await carsCollection.insertOne(cars);
+      res.send(products);
+    });
 
-      //update
-      app.put('/cars/:id', async(req,res) =>{
-        const id =req.params.id;
-        const oldQuantity =parseInt(req.query.oldQuantity);
-        const updateCars = req.body;
-        const filter = {_id: ObjectId(id)};
-        const options ={upsert: true};
-        const updateDoc = {
-          $set : {
-            quantity: parseInt(updateCars.quantity) + parseInt(oldQuantity)
-          }
-        };
-        const result = await carsCollection.updateOne(filter,updateDoc,options);
-        res.send(result);
-      })
+    //single data
 
-      //decrising one by one
+    app.get("/data/:id", async (req, res) => {
+      const id = req.params.id;
+      const querry = { _id: ObjectId(id) };
+      const result = await carsCollection.findOne(querry);
+      res.send(result);
+    });
 
-    app.put("/cars/:id", async (req, res) => {
+   
+
+    //decrising one by one
+
+    app.put("/datas/:id", async (req, res) => {
       const id = req.params.id;
       const oldQuantity = parseInt(req.query.oldQuantity);
       const filter = { _id: ObjectId(id) };
@@ -64,29 +65,11 @@ async function run () {
           quantity: parseInt(oldQuantity) - 1,
         },
       };
-      const result = await dataCollection.updateOne(filter, uDoc, options);
+      const result = await carsCollection.updateOne(filter, uDoc, options);
       res.send(result);
     });
-
-    //post 
-    app.post("/cars", async (req, res) => {
-      const cars = req.body;
-      const products = await carsCollection.insertOne(cars);
-      res.send(products);
-    });
-
-     //delete
-     app.delete("/cars/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const products = await carsCollection.deleteOne(query);
-      console.log(products);
-      res.send(products);
-    });
-    }
-    finally{
-
-    }
+  } finally {
+  }
 }
 run().catch(console.dir);
 
